3d17c96987cb2cca6000c75de29f55d6f8f0c590
KaktusPai <59810116+KaktusPai@users.noreply.github.com>
2020-07-25 15:49:16 +0200

    Repushing with OG name because I'm just stupid

---
+++ BPW Puzzle Game/Assets/BallMechanic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMechanic : MonoBehaviour
+{
+
+    public GameObject player;
+    public Transform ball1;
+    public Transform ball2;
+
+    void Update()
+    {
+
+        if (Input.GetKey("e"))
+        {
+            player.transform.position =
+            (ball1.transform.position + ball2.transform.position) / 2; 
+        }
+    }
+}
---
+++ BPW Puzzle Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public GameObject crosshair;
+
+    Vector2 movement;
+
+    // Input
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+    }
+
+    //Movement
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
