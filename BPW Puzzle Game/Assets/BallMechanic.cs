8937676ba0c9b4b05af5f7b2fcd5ad7932d60e4f
KaktusPai <jessebrolsma@gmail.com>
2020-07-27 15:50:44 +0200

    Can now shoot balls
    hell yeah

--- BPW Puzzle Game/Assets/BallMechanic.cs
+++ BPW Puzzle Game/Assets/BallMechanic.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class BallMechanic : MonoBehaviour
 {
-
     public GameObject player;
     public Transform ball1;
     public Transform ball2;
 
+    public Transform firePoint;
+    public GameObject ballPrefab;
+
+    public float ballForce = 20f;
+
     void Update()
     {
-
         if (Input.GetKey("e"))
         {
             player.transform.position =
             (ball1.transform.position + ball2.transform.position) / 2; 
         }
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Throw();
+        }
     }
+
+    void Throw()
+    {
+        GameObject ball = Instantiate(ballPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = ball.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * ballForce, ForceMode2D.Impulse);
+    }
+
+
 }
--- BPW Puzzle Game/Assets/Scripts/PlayerMovement.cs
+++ BPW Puzzle Game/Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,11 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
-    public GameObject crosshair;
+    public Rigidbody2D throwrb;
+    public Camera cam;
 
     Vector2 movement;
+    Vector2 mousePos;
 
     // Input
     void Update()
@@ -18,11 +20,17 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     //Movement
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        throwrb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - throwrb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        throwrb.rotation = angle;
     }
 }
